c94d9040c541c0f93e316af3925ebe80a95273bb
codewithkushagra <kushagraagra008@gmail.com>
2023-03-01 22:12:55 +0530

    removed base.html

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
