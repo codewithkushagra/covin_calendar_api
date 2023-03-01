4c4e2fc9b2a43cbb664f2256ae5b94a85ebc4041
codewithkushagra <kushagraagra008@gmail.com>
2023-03-02 02:08:44 +0530

    change url

--- calender_api/constants/constants.py
+++ calender_api/constants/constants.py
@@ -8,9 +8,9 @@ GOOGLE_USERINFO_ENDPOINT = 'https://www.googleapis.com/oauth2/v1/userinfo'
 GOOGLE_CALENDAR_ENDPOINT = 'https://www.googleapis.com/calendar/v3/calendars/primary/events'
 
 
-# CLIENT_ID = "112358493438-edfr9undt7k89q3k49532nd1k0r04oph.apps.googleusercontent.com"
-CLIENT_ID = "112358493438-q3bvo7skq7toisuolu8qcbda0bvck6qn.apps.googleusercontent.com"
-# CLIENT_SECRET = "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"
-CLIENT_SECRET = "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"
-# REDIRECT_URI = "https://api.kushagraagarwal.software/rest/v1/calendar/redirect"
-REDIRECT_URI = "http://127.0.0.1:8000/rest/v1/calendar/redirect"
+CLIENT_ID = "112358493438-edfr9undt7k89q3k49532nd1k0r04oph.apps.googleusercontent.com"
+# CLIENT_ID = "112358493438-q3bvo7skq7toisuolu8qcbda0bvck6qn.apps.googleusercontent.com"
+CLIENT_SECRET = "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"
+# CLIENT_SECRET = "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"
+REDIRECT_URI = "https://api.kushagraagarwal.software/rest/v1/calendar/redirect"
+# REDIRECT_URI = "http://127.0.0.1:8000/rest/v1/calendar/redirect"
